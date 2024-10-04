c26676b924b2663b9c6abfddf619660d05f451af
mart <mart.ako.paris@tktk.ee>
2024-10-04 21:37:21 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - ||
+
+//true || true --> true
+//true || fasle --> true
+//false || true --> true
+//false || false --> false
